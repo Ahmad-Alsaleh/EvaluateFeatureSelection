dc93c8e3f868d914d2d0117db4e9f4ff497b31a4
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2024-01-30 10:16:58 +0400

    fix: replace variable name AUC.values with auc_values

--- R/auc_plot.R
+++ R/auc_plot.R
@@ -2,9 +2,9 @@
 # Prerequisites: the column that contains the size of the subsets
 # is called "subset.size"
 # Arguments:
-# `AUC.values`: a data.frame returned from compute_aucs()
+# `auc_values`: a data.frame returned from compute_aucs()
 # `model.name`: a string contains the name of the model
-# that was used to obtain `AUC.values`
+# that was used to obtain `auc_values`
 auc_plot <- function(auc_values, model_name) {
   melted_auc_values <- reshape2::melt(auc_values, id = "subset.size")
   plot <- ggplot2::ggplot(
@@ -18,9 +18,8 @@ auc_plot <- function(auc_values, model_name) {
       hjust = 0.5,
       face = "bold"
     )) +
-    # ggplot2::scale_x_continuous(trans = reverselog_trans()) +
     ggplot2::scale_x_reverse(labels = function(x) {
-      glue::glue("{x}\n({round(x / max(AUC.values$subset.size) * 100, 1)}%)")
+      glue::glue("{x}\n({round(x / max(auc_values$subset.size) * 100, 1)}%)")
     }) +
     ggplot2::geom_line(linetype = "dotdash", linewidth = 1) +
     ggplot2::labs(linetype = "FS Method", color = "FS Method")
